4827785c96e7dadffdd3ccf93c651b781c63d5f0
Ana Icu <25004137@galileo.edu>
2026-03-26 21:18:26 -0600

    Semana 6 v1

--- routes/users.js
+++ routes/users.js
@@ -13,11 +13,9 @@ router.post('/register', async function(req, res, next) {
   try {
     const { username, password } = req.body;
 
-    // Generamos un salt y hash con bcrypt
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Guardamos en la base de datos
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
 
@@ -34,16 +32,20 @@ router.post('/login', async function(req, res, next) {
     const user = await User.findOne({ username });
     if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
 
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ error: "Contraseña incorrecta" });
 
+
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-    res.cookie('habitToken', token, {
-      httpOnly: false, // Previene acceso desde JavaScript (XSS)
-      secure: false, // Solo en HTTPS en producción
-      sameSite: "lax", // Evita envío en otros sitios
-      maxAge: 7 * (24) * 60 * 60 * 1000 // 7 días de duración
-  });
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    res.cookie("habitToken", token, {
+      httpOnly: false,
+      secure: isProduction,
+      sameSite: isProduction ? 'none' : 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
     res.json({ message: "Inicio de sesión exitoso", token });
 } catch (error) {
     res.status(500).json({ error: "Error en el login", "description":error.toString() });
