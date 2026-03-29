90fbf67b926edd9735f4916ff4a1ef2481fbfe5b
Ana Icu <25004137@galileo.edu>
2026-03-29 17:58:02 -0600

    Actualización

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 var app = express();
 app.use(cors({
-  origin: isProduction 
+  origin: isProduction
   ? "https://habitosatomicos-frontend-25004137.onrender.com"
   : "http://localhost:5173",
   credentials: true
